8bc0a29068040472fec93b0dfc16fd64ebdd266d
Manel Arbiol <Manel>
2024-10-03 00:34:30 +0200

    Innit Inicial

---
+++ T1Activitats/20.CountDown.cs
@@ -0,0 +1,33 @@
+﻿using System;
+/*
+ * Author: Manel Arbiol Calpe
+ * T1. Activits. Introducció a la Programació
+ * 29/09/2024
+ * Exercici 20  .
+ *  20. Implementa un algorisme que demani a l'usuari que introdueixi un valor enter (N) i es mostri per pantalla un compte enrere de N fins a 1.
+ *
+ *  Entrada d’exemple
+ *     5
+ *   Sortida d’exemple
+ *     5 4 3 2 1
+ */
+
+namespace T1Activitats
+{
+    public class CountDownNumbers
+    {
+        public static void Main(string[] args)
+        {
+                        
+            const string WRITE_NUMBER = "Introdueix un número enter: ";
+
+            Console.WriteLine(WRITE_NUMBER);
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = number; i > 0; i--)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }      
+}
