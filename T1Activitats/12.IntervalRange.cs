1220f22c69811d1a96125e3901dcbe4f252fc5fc
Manel Arbiol <manel.arbiol.7e7@itb.cat>
2024-10-07 11:53:33 +0200

    07/10/2024

---
+++ T1Activitats/12.IntervalRange.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+/*
+ * Author: Manel Arbiol Calpe
+ * T1. Activits. Introducció a la Programació
+ * 3/10/2024
+ * Exercici 12  .
+ * 12. Implementa el pseudocodi d’un algorisme que informi a l’usuari si el valor 
+ * que ha introduït per teclat és dins de l’interval (5, 50).
+ */
+
+
+
+namespace T1Activitats
+{
+    public class IntervalRange
+    {
+        static void Main()
+
+        //constants
+        {
+            const int MAX_RANGE = 50;
+            const int MIN_RANGE = 5;
+            const string ENTER_NUM_TXT = "Introdueix un número entre 5 i 50: ";
+            const string CORRECT_NUM_TXT = "El número introduït és correcte.";
+            const string INCORRECT_NUM_TXT = "El número introduït no està dins del interval.";
+
+            //variables
+
+            int num;
+
+            Console.WriteLine(ENTER_NUM_TXT);
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num >= MIN_RANGE && num <= MAX_RANGE)
+            {
+                Console.WriteLine(CORRECT_NUM_TXT);
+            }
+            else
+            {
+                Console.WriteLine(INCORRECT_NUM_TXT);
+            }
+        }
+    }
+}
---
+++ T1Activitats/13.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+/*
+ * Author: Manel Arbiol Calpe
+ * T1. Activits. Introducció a la Programació
+ * 3/10/2024
+ * Exercici 13  .
+ * 13.  Implementa el pseudocodi d’un programa que demani un nombre enter per teclat que estigui dins de l’interval (2, 30). L’usuari disposarà de 3 intents per a indicar un nombre de l’interval. 
+ * En cas contrari, el programa indicarà que ha perdut i finalitzarà.
+ */
+
+
+
+namespace T1Activitats
+{
+    public class IntervalRangeWithTrys
+    {
+        static void Main()
+
+        //constants
+        {
+            const int MAX_RANGE = 30;
+            const int MIN_RANGE = 2;
+            const int MAX_TRIES = 3;
+            const string ENTER_NUM_TXT = "Introdueix un número entre 3 i 30: ";
+            const string CORRECT_NUM_TXT = "El número introduït és correcte.";
+            const string INCORRECT_NUM_TXT = "El número introduït no està dins del interval.";
+            const string LOST_TRIES_TXT = "Has esgotat tots els intents.";
+            const string TRYS_LEFT_TXT = "$Et queden {0} intents.";
+            const string WIN_TXT = "Has guanyat!";
+
+            //variables
+
+            int num, numTryes = 0;
+
+
+            //code
+
+            do
+            {
+                numTryes++;
+                Console.Write(${ ENTER_NUM_TXT + }
+                T
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num >= MIN_RANGE && num <= MAX_RANGE)
+                {
+                    Console.WriteLine(CORRECT_NUM_TXT);
+                    Console.WriteLine(WIN_TXT);
+                }
+                else
+                {
+                    Console.WriteLine(INCORRECT_NUM_TXT);
+                }
+            } while ((num < MIN_RANGE || num > MAX_RANGE) && numTryes < MAX_TRIES);
+            Console.ReadLine();
+            if (numTryes == MAX_TRIES)
+            {
+                Console.WriteLine(LOST_TRIES_TXT);
+            }
+        }
+    }
+}
